64901fc1fc22bd2048ef7fdbf31665e11ff5ac11
Scott Ko <swko1017@naver.com>
2022-01-19 18:32:44 +0900

    worker counter clearInterval 수정

--- src/workers/worker.js
+++ src/workers/worker.js
@@ -1,23 +1,43 @@
 // import { exposeWorker } from 'react-hooks-worker';
 
 // const fib = (i) => (i <= 1 ? i : fib(i - 1) + fib(i - 2));
+let intervalId = null;
 
 onmessage = (evt) => {
   // console.log(evt);
   // postMessage(fib(evt.data));
-  let intervalID = null;
   let msg = evt.data;
-  console.log(msg);
-  intervalID = setInterval(() => {
-    if (msg.active && !msg.pause) {
+  // console.log(msg);
+
+  if(msg.active && !msg.pause) {
+    intervalId = setInterval(() => {
       msg.count = msg.count + 1;
       postMessage(msg);
 
-      if (msg.count >= msg.limit || msg.pause) {
-        clearInterval(intervalID);
+      if (msg.count >= msg.limit) {
+        clearInterval(intervalId);
         postMessage(msg);
       }
-    }
-  }, 1000);
+    }, 1000);
+  } else {
+    clearInterval(intervalId);
+    
+  }
+
+
+  // if (msg.pause || !msg.active) {
+  //   clearInterval(intervalId);
+  //   console.log(msg);
+  // } else {
+  //   intervalId = setInterval(() => {
+  //     msg.count = msg.count + 1;
+  //     postMessage(msg);
+
+  //     if (msg.count >= msg.limit) {
+  //       clearInterval(intervalId);
+  //       postMessage(msg);
+  //     }
+  //   }, 1000);
+  // }
 };
 // exposeWorker(fib);
