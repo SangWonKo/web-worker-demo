9f0ee10c8dd503aa553ac2898219c7412555ed9a
Scott Ko <swko1017@naver.com>
2022-01-20 18:56:16 +0900

    framer-motion 버튼 적용 및 AnimatePresence exit animation 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import { useCallback, useRef, useState } from "react";
+import { motion, Variants, Transition, AnimatePresence } from "framer-motion";
+import styled from "styled-components";
 import "./App.css";
 
 // import { useWorker } from "react-hooks-worker";
+
+const MotionText = styled(motion.div)`
+  font-size: 60px;
+  font-family: "Montserrat Alternates";
+  font-weight: 600;
+  line-height: 40px;
+  text-align: center;
+`;
+
+const MotionButton = styled(motion.button)`
+  border: none;
+  cursor: pointer;
+  background-color: #fff;
+  color: #5e5e5e;
+  border-radius: 36px;
+  outline: none;
+  margin: 0;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-top: 20px;
+  font-family: "Montserrat Alternates";
+  font-size: 42px;
+  letter-spacing: -2px;
+  font-weight: 600;
+  line-height: 40px;
+  position: relative;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  box-shadow: 0px 40px 80px 0px rgba(0, 0, 0, 0.05),
+    inset 0px -10px 20px 0px rgba(0, 0, 0, 0.05),
+    0px 10px 20px 0px rgba(0, 0, 0, 0.05);
+`;
 
 const createWorker = () =>
   new Worker(new URL("./workers/worker.js", import.meta.url));
@@ -30,9 +66,7 @@ function App() {
     (flg) => () => {
       // console.log(counter.count);
       const newState =
-        flg !== "reset"
-          ? { ...counter, [flg]: !counter[flg] }
-          : defaultState;
+        flg !== "reset" ? { ...counter, [flg]: !counter[flg] } : defaultState;
       console.log(newState);
       setCounter(newState);
       worker.current.postMessage(newState);
@@ -52,16 +86,29 @@ function App() {
 
   return (
     <>
-      <div>Counter: {counter.count}</div>
+      <AnimatePresence>
+        <MotionText
+          key={counter.count}
+          exit={{ y: -75, opacity: 0, position: "absolute" }}
+          initial={{ y: 150, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            ease: "easeOut",
+            duration: 1,
+          }}
+        >
+          {counter.count}
+        </MotionText>
+      </AnimatePresence>
       {!counter.active && (
-        <button onClick={handleCount("active")}>start</button>
+        <MotionButton onClick={handleCount("active")}>start</MotionButton>
       )}
       {counter.active && (
-        <button onClick={handleCount("pause")}>
+        <MotionButton onClick={handleCount("pause")}>
           {counter.pause ? "resume" : "pause"}
-        </button>
+        </MotionButton>
       )}
-      {counter.pause && <button onClick={handleCount("reset")}>reset</button>}
+      {counter.pause && <MotionButton onClick={handleCount("reset")}>reset</MotionButton>}
     </>
   );
 }
--- src/workers/worker.js
+++ src/workers/worker.js
@@ -2,12 +2,8 @@
 
 // const fib = (i) => (i <= 1 ? i : fib(i - 1) + fib(i - 2));
 let intervalId = null;
-
 onmessage = (evt) => {
-  // console.log(evt);
-  // postMessage(fib(evt.data));
   let msg = evt.data;
-  // console.log(msg);
 
   if(msg.active && !msg.pause) {
     intervalId = setInterval(() => {
@@ -23,21 +19,5 @@ onmessage = (evt) => {
     clearInterval(intervalId);
     
   }
-
-
-  // if (msg.pause || !msg.active) {
-  //   clearInterval(intervalId);
-  //   console.log(msg);
-  // } else {
-  //   intervalId = setInterval(() => {
-  //     msg.count = msg.count + 1;
-  //     postMessage(msg);
-
-  //     if (msg.count >= msg.limit) {
-  //       clearInterval(intervalId);
-  //       postMessage(msg);
-  //     }
-  //   }, 1000);
-  // }
 };
 // exposeWorker(fib);
